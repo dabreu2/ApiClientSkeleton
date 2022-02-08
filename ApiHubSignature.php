b41d1a7415e1916e8e3cf0166d1a2a188d7d77c7
fedegara <fedegara91@gmail.com>
2022-02-08 15:46:44 -0300

    Added JSON_UNESCAPED_SLASHES to json_encode when generate X-HUB-SIGNATURE to avoid problems slash escapes in  parameters

--- ApiHubSignature.php
+++ ApiHubSignature.php
@@ -45,7 +45,7 @@ class ApiHubSignature
         }
 
         $carrier['x-hub-signature'] = $this->getSignature(
-            json_encode($content,JSON_UNESCAPED_UNICODE)
+            json_encode($content,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES)
         );
 
         return true;
